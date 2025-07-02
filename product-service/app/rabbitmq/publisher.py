3146f3552480571cf3da0b6227fb8b96771899e7
Ziad <68874104+Ziad-I@users.noreply.github.com>
2025-07-02 20:15:46 +0300

    feat: add exchange type when creating exchange in product-service

--- product-service/app/rabbitmq/publisher.py
+++ product-service/app/rabbitmq/publisher.py
@@ -1,4 +1,4 @@
-from aio_pika import Channel, Message
+from aio_pika import Channel, Message, ExchangeType
 
 from app.core.config import settings
 
@@ -6,6 +6,8 @@ from app.core.config import settings
 async def publish_message(channel: Channel, routing_key: str, message: str) -> None:
     """Publish a message to a RabbitMQ exchange."""
     exchange = await channel.declare_exchange(
-        settings.RABBITMQ_EXCHANGE_NAME, durable=True
+        settings.RABBITMQ_EXCHANGE_NAME,
+        type=ExchangeType.DIRECT,
+        durable=True,
     )
     await exchange.publish(Message(body=message.encode()), routing_key=routing_key)
