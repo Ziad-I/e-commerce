2ad91cbb3267f5a6d996d519006b7ce9d05010fc
Ziad <68874104+Ziad-I@users.noreply.github.com>
2025-07-16 19:45:09 +0300

    feat: add validate endpoint to validate user access token

--- user-service/app/api/v1/auth.py
+++ user-service/app/api/v1/auth.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from app.models.user import User
 from app.schemas.user import UserCreate, UserRead
 from app.auth.backend import jwt_authentication_backend, cookie_authentication_backend
-from app.auth.dependency import fastapi_users
+from app.auth.dependency import fastapi_users, current_active_user
 
 router = APIRouter()
 
@@ -28,3 +29,9 @@ router.include_router(
     fastapi_users.get_verify_router(UserRead),
     tags=["Auth"],
 )
+
+
+@router.post("/validate", tags=["Auth"])
+async def validate_user(user: User = Depends(current_active_user)) -> UserRead:
+    """Endpoint to validate the current active user."""
+    return user
