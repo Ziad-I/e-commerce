f142f1cdcd582f753d72c7e59aa3f72348b2cf46
Ziad <68874104+Ziad-I@users.noreply.github.com>
2025-07-16 15:28:27 +0300

    refactor: make the auth router a sub router to the user router
    this change is needed to make the api consistent across all services
    
    All services (and sub‑routers like auth) stay logically grouped under /api/v1/<service>/…  and Nginx no longer needs special cases
    
    - /api/v1/users/me → http://user-service/api/v1/users/me
    
    - /api/v1/users/auth/login → http://user-service/api/v1/users/auth/login
    
    - /api/v1/products/42 → http://product-service/api/v1/products/42
    
    etc..

--- user-service/app/api/router.py
+++ user-service/app/api/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from app.api.v1.auth import router as auth_router
 from app.api.v1.user import router as user_router
 
+user_router.include_router(auth_router, prefix="/auth")
+
 v1_router = APIRouter()
-v1_router.include_router(auth_router, prefix="/auth", tags=["Auth"])
-v1_router.include_router(user_router, prefix="/users", tags=["Users"])
+v1_router.include_router(user_router, prefix="/users")
